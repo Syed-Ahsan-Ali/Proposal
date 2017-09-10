b512b62a9882e902204c06a441a0b8691cad906b
ahsan.ali <ahsan.ali@itboost.com>
2017-09-10 20:37:30 +0500

    changes

--- src/components/Proposal/DropDownComp.jsx
+++ src/components/Proposal/DropDownComp.jsx
@@ -1,16 +1,35 @@
 import {Component} from "react";
-export const DropDownComp extends Component{
-  constructor(){
-    super();
+import {connect} from "react-redux";
+export class DropDownComp extends Component{
+  constructor(props){
+    super(props);
   }
   render(){
     return(
       <div id="colvisbtn">
-        <div data-toggle="tooltip" data-placement="top" title="Select Skill">
-          <button></button>
-          <div></div>
+        <div data-toggle="tooltip" data-placement="top" title="Select Skill" className="dropdown pull-right">
+          <button className="dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown">
+            <a href="#">
+              <img src="./../../../img/icon_listdd.png"/>
+            </a>
+          </button>
+          <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <div>
+              <label>Test
+              <input type="checkbox"/>
+              </label>
+            </div>
+          </div>
         </div>
       </div>
     );
   }
 }
+const mapStateToProps=(state)=>({
+
+});
+const mapDispatchToProps=(dispatch)=>({
+
+});
+export const DropDownContainer=
+  connect(mapStateToProps,mapDispatchToProps)(DropDownComp)
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,5 @@
 //import * as React from "react";
+import {DropDownContainer} from "./components/Proposal/DropDownComp";
 import {kaseyaContainer as KaseyaContainer} from "./components/integrations/kaseya/kaseyaContainer";
 import {Route,BrowserRouter as Router} from "react-router-dom";
 // import {domainTracker as DomainTracker} from "./components/company/domainTracker/domainTrackerContainer";
@@ -13,7 +14,7 @@ export const routes = () => (
       {/*<Switch>*/}
       {/*<Route path="/" component={Root} />*/}
       <Route path="/integration/kaseya/view" component={KaseyaContainer}  />
-
+      <Route path="/Proposal" component={DropDownContainer}  />
       {/*<Route path="/company/domainTracker/:domain/view" component={DomainTracker}  />*/}
       {/*</Switch>*/}
     </div>
